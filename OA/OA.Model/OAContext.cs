56c03e4b7b174426a6b3672df35cd0b2b3f64857
WxhShine <790875216@qq.com>
2018-09-12 20:47:32 +0800

    新增项目框架

---
+++ OA/OA.Model/OAContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.Model {
+    public class OAContext : DbContext {
+        public OAContext() : base("name = OAContext") {}
+        
+        public DbSet<>
+    }
+}
---
+++ OA/OA.Model/UserInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.Model {
+    public class UserInfo {
+        public int Id { get; set; }
+        public string UName { get; set; }
+        public string UPwd { get; set; }
+        public DateTime SubTime { get; set; }
+        public short DelFlag { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public string Remark { get; set; }
+        public string Sort { get; set; }
+    }
+}
